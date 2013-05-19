3e205d3c77400f9eef7f68575d49ee01dcf0115d
Azat Abdulvaliev <atik1990@ya.ru>
2013-05-19 12:51:49 +0400

    Rope as behaviour and test for it

--- rope/rope.erl
+++ rope/rope.erl
@@ -1,242 +1,238 @@
-%%% туду 
-%%% 1. переделать все регулярки на бинари вместо строк
-%%% 2. Воткнуть сюда re_transition
-%%% 3. Тест ire
-
-
 -module(rope).
--export([split/2, merge/2, empty_rope/0, binary2rope/1, rope2binary/1, length/1]).
+-export([leaf_rope/2, get_cache/1, split/3, merge/3, flatten/2, length/1]).
 
 
--define(Leaf_size, 5). % leaf length can be no more than (2 * ?Leaf_size - 1)
+-type key() :: number().
+-type value() :: term().
 
+% some cache for every rope
+-type cache() :: term().
 
-
-
--type key() :: term().
--type value() :: binary().
-
-% we can not tag them because they are all of different lengths
+% params used to generalize behaviour
+% leaf length can be no more than (2 * LeafLength - 1)
+-type params() :: {Module::atom(), LeafLength::number()}.
 
 -type rope_node() :: 
-				{Key1::key(), 
-					Key2::key(), 
+				{Key1::key(), Key2::key(), 
 					Tree1::not_empty_rope(), 
 					Tree2::not_empty_rope(), 
 					Tree3::not_empty_rope(),
-					TransDict::cache()}
+					cache()}
 			  | {Key::key(), 
 			  		Tree1::not_empty_rope(), 
 			  		Tree2::not_empty_rope(),
-			  		TransDict::cache()}
-			  | {value(), TransDict::cache()}.
+			  		cache()}
+			  | {value(), cache()}.
 
 -type not_empty_rope() :: {Height::integer(), Length::key(), rope_node()}.
 
 -type rope() :: empty_rope | not_empty_rope().
 
-% cache of transition function value for sub-rope
--type cache() :: term().
+
+-callback length(value()) -> number().
+-callback cache(value()) -> cache().
+% length(Value1) + length(Value2) = length(merge_values(Value1, Value2))
+-callback merge_values(value(), value()) -> value().
+% if {Value1, Value2} = split_value(Value) 
+% then length(Value1) + length(Value2) = length(Value)
+-callback split_value(value(), number()) -> {value(), value()}.
+-callback merge_caches(cache(), cache()) -> cache().
 
 
+% public
+-spec leaf_rope(value(), params()) -> rope().
+
+leaf_rope(Value, {Module, _}) ->
+	Len = Module:length(Value),
+	Cache = Module:cache(Value),
+	{0, Len, {Value, Cache}}.
 
 
--spec split(rope(), Position::key()) -> {Rope1::rope(), Rope2::rope()}.
+% public
+-spec get_cache(rope()) -> cache() | undefined.
+
+get_cache(empty_rope) -> undefined;
+get_cache({_, _, {_, Cache}}) -> Cache;
+get_cache({_, _, {_, _, _, Cache}}) -> Cache;
+get_cache({_, _, {_, _, _, _, _, Cache}}) -> Cache.
+
+
+% public
+-spec split(rope(), Position::key(), params()) -> {Rope1::rope(), Rope2::rope()}.
 %% splits Tree into two trees,
 %% merge(Tree1, Tree2) == Tree,
 %% length of Tree1 == Position.
 
 % to be consistent
-split(empty_rope, _) ->
+split(empty_rope, _, _) ->
 	{empty_rope, empty_rope};
 % miss
-split(Tree, Pos) when Pos =< 0 -> 
+split(Tree, Pos, _) when Pos =< 0 -> 
 	{empty_rope, Tree};
-split(Tree = {_, Len, _}, Pos) when Pos >= Len -> 
+split(Tree = {_, Len, _}, Pos, _) when Pos >= Len -> 
 	{Tree, empty_rope};
 % 2-node
-split({_, _, {Key, Tree1, Tree2, _}}, Pos) when Pos < Key -> 
-	{Tree11, Tree12} = split(Tree1, Pos),
-	{Tree11, merge(Tree12, Tree2)};
-split({_, _, {Key, Tree1, Tree2, _}}, Pos) when Pos >= Key -> 
-	{Tree21, Tree22} = split(Tree2, Pos - Key),
-	{merge(Tree1, Tree21), Tree22};
+split({_, _, {Key, Tree1, Tree2, _}}, Pos, Params) when Pos < Key -> 
+	{Tree11, Tree12} = split(Tree1, Pos, Params),
+	{Tree11, merge(Tree12, Tree2, Params)};
+split({_, _, {Key, Tree1, Tree2, _}}, Pos, Params) when Pos >= Key -> 
+	{Tree21, Tree22} = split(Tree2, Pos - Key, Params),
+	{merge(Tree1, Tree21, Params), Tree22};
 % 3-node
-split({_, _, {Key1, _, Tree1, Tree2, Tree3, _}}, Pos) when Pos < Key1 -> 
-	{Tree11, Tree12} = split(Tree1, Pos),
-	{Tree11, merge(Tree12, merge(Tree2, Tree3))};
-split({_, _, {Key1, Key2, Tree1, Tree2, Tree3, _}}, Pos) when Pos < Key2 -> 
-	{Tree21, Tree22} = split(Tree2, Pos - Key1),
-	{merge(Tree1, Tree21), merge(Tree22, Tree3)};
-split({_, _, {_, Key2, Tree1, Tree2, Tree3, _}}, Pos) when Pos >= Key2 -> 
-	{Tree31, Tree32} = split(Tree3, Pos - Key2),
-	{merge(Tree1, merge(Tree2, Tree31)), Tree32};
+split({_, _, {Key1, _, Tree1, Tree2, Tree3, _}}, Pos, Params) when Pos < Key1 -> 
+	{Tree11, Tree12} = split(Tree1, Pos, Params),
+	{Tree11, merge(Tree12, merge(Tree2, Tree3, Params), Params)};
+split({_, _, {Key1, Key2, Tree1, Tree2, Tree3, _}}, Pos, Params) when Pos < Key2 -> 
+	{Tree21, Tree22} = split(Tree2, Pos - Key1, Params),
+	{merge(Tree1, Tree21, Params), merge(Tree22, Tree3, Params)};
+split({_, _, {_, Key2, Tree1, Tree2, Tree3, _}}, Pos, Params) when Pos >= Key2 -> 
+	{Tree31, Tree32} = split(Tree3, Pos - Key2, Params),
+	{merge(Tree1, merge(Tree2, Tree31, Params), Params), Tree32};
 % leaf
-split({0, Len, Leaf}, Pos) ->
-	{Val, _Cache} = Leaf,
-	<<Val1:Pos/binary, Val2/binary>> = Val,
-	{
-		{0, Pos, 
-			{Val1, compute_cache(Val1)}}, 
-		{0, Len - Pos, 
-			{Val2, compute_cache(Val2)}}
-	}.
+split({0, Len, Leaf}, Pos, Params) ->
+	{Val, _} = Leaf,
+	{Module, _} = Params,
+	{Val1, Val2} = Module:split_value(Val, Pos),
+	Leaf1 = {Val1, Module:cache(Val1)},
+	Leaf2 = {Val2, Module:cache(Val2)},
+	{{0, Pos, Leaf1}, {0, Len - Pos, Leaf2}}.
 
 
 
 
--spec merge(rope(), rope()) -> rope().
+-spec merge(rope(), rope(), params()) -> rope().
 
 % empty
-merge(empty_rope, Tree2) -> Tree2;
-merge(Tree1, empty_rope) -> Tree1;
+merge(empty_rope, Tree2, _) -> Tree2;
+merge(Tree1, empty_rope, _) -> Tree1;
 
 % two leafs
-merge({0, Len1, Leaf1}, {0, Len2, Leaf2}) ->
+merge({0, Len1, Leaf1}, {0, Len2, Leaf2}, Params) ->
 	{Val1, Cache1} = Leaf1,
 	{Val2, Cache2} = Leaf2,
-	case (Len1 + Len2 < 2 * ?Leaf_size) of 
+	{Module, LeafLength} = Params,
+	case (Len1 + Len2 < 2 * LeafLength) of 
 		true ->
-			NewVal = {<<Val1/binary, Val2/binary>>, merge_caches(Cache1, Cache2)},
-			{0, Len1 + Len2, NewVal}; % tree with leaf
+			Leaf = {Module:merge_values(Val1, Val2), Module:merge_caches(Cache1, Cache2)},
+			{0, Len1 + Len2, Leaf}; % tree with leaf
 		false ->
-			<<NewVal1:?Leaf_size/binary, NewVal2/binary>> = <<Val1/binary, Val2/binary>>,
-			{1, Len1 + Len2, % tree
-				{?Leaf_size + 1, % 2-node
-					{0, ?Leaf_size, {NewVal1, compute_cache(NewVal1)}}, % tree with leaf
-					{0, Len1 + Len2 - ?Leaf_size, {NewVal2, compute_cache(NewVal2)}}, % tree with leaf
-					merge_caches(Cache1, Cache2)}} 
+			{NewVal1, NewVal2} = Module:split_value(Module:merge_values(Val1, Val2), LeafLength),
+			NewCache1 = Module:cache(NewVal1),
+			NewCache2 = Module:cache(NewVal2),
+			NewLen1 = Module:length(NewVal1),
+			NewLen2 = Module:length(NewVal2),
+			{1, NewLen1 + NewLen2, % tree
+				{NewLen1 + 1, % 2-node
+					{0, NewLen1, {NewVal1, NewCache1}}, % tree with leaf
+					{0, NewLen2, {NewVal2, NewCache2}}, % tree with leaf
+					Module:merge_caches(NewCache1, NewCache2)}} 
 	end;
 
 % equal height
-merge(Tree1 = {H1, Len1, Node1}, Tree2 = {H2, Len2, Node2}) when H1 == H2 ->
-	Cache1 = extract_tree_cache(Tree1), % ugly, use records
+merge(Tree1 = {H1, Len1, Node1}, Tree2 = {H2, Len2, Node2}, Params) when H1 == H2 ->
+	Cache1 = extract_tree_cache(Tree1),
 	Cache2 = extract_tree_cache(Tree2),
-	{H1 + 1, Len1 + Len2, {Len1, {H1, Len1, Node1}, {H2, Len2, Node2}, merge_caches(Cache1, Cache2)}};
+	{Module, _} = Params,
+	{H1 + 1, Len1 + Len2, {Len1, {H1, Len1, Node1}, {H2, Len2, Node2}, Module:merge_caches(Cache1, Cache2)}};
 
 % diff == 1
 % 2-node + node = 3-node
-merge({H1, Len1, {Key1, Tree11, Tree12, Cache1}}, Tree2 = {H2, Len2, Node2}) when H1 == H2 + 1 ->
+merge({H1, Len1, {Key1, Tree11, Tree12, Cache1}}, Tree2 = {H2, Len2, Node2}, Params) when H1 == H2 + 1 ->
 	Cache2 = extract_tree_cache(Tree2),
-	{H1, Len1 + Len2, {Key1, Len1, Tree11, Tree12, {H2, Len2, Node2}, merge_caches(Cache1, Cache2)}};
+	{Module, _} = Params,
+	{H1, Len1 + Len2, {Key1, Len1, Tree11, Tree12, {H2, Len2, Node2}, Module:merge_caches(Cache1, Cache2)}};
 % node + 2-node = 3-node
-merge(Tree1 = {H1, Len1, Node1}, {H2, Len2, {Key2, Tree21, Tree22, Cache2}}) when H1 + 1 == H2 ->
+merge(Tree1 = {H1, Len1, Node1}, {H2, Len2, {Key2, Tree21, Tree22, Cache2}}, Params) when H1 + 1 == H2 ->
 	Cache1 = extract_tree_cache(Tree1),
-	{H2, Len1 + Len2, {Len1, Len1 + Key2, {H1, Len1, Node1}, Tree21, Tree22, merge_caches(Cache1, Cache2)}};
+	{Module, _} = Params,
+	{H2, Len1 + Len2, {Len1, Len1 + Key2, {H1, Len1, Node1}, Tree21, Tree22, Module:merge_caches(Cache1, Cache2)}};
 % 3-node + small
-merge({H1, Len1, {_, _, Tree11, Tree12, Tree13, Cache1}}, Tree2 = {H2, Len2, _}) when H1 == H2 + 1 ->
+merge({H1, Len1, {_, _, Tree11, Tree12, Tree13, Cache1}}, Tree2 = {H2, Len2, _}, Params) when H1 == H2 + 1 ->
 	Cache11 = extract_tree_cache(Tree11),
 	Cache12 = extract_tree_cache(Tree12),
 	Cache13 = extract_tree_cache(Tree13),
 	Cache2 = extract_tree_cache(Tree2),
+	{Module, _} = Params,
 	{_, Len11, _} = Tree11,
 	{_, Len12, _} = Tree12,
 	{_, Len13, _} = Tree13,
 	{H1 + 1, Len1 + Len2, % tree
 		{Len11 + Len12, % 2-node
-			{H1, Len11 + Len12, {Len11, Tree11, Tree12, merge_caches(Cache11, Cache12)}}, % tree with 2-node
-			{H1, Len13 + Len2, {Len1, Tree13, Tree2, merge_caches(Cache13, Cache2)}}, % tree with 2-node
-			merge_caches(Cache1, Cache2)}};
+			{H1, Len11 + Len12, {Len11, Tree11, Tree12, Module:merge_caches(Cache11, Cache12)}}, % tree with 2-node
+			{H1, Len13 + Len2, {Len1, Tree13, Tree2, Module:merge_caches(Cache13, Cache2)}}, % tree with 2-node
+			Module:merge_caches(Cache1, Cache2)}};
 % small + 3-node
-merge(Tree1 = {H1, Len1, _}, {H2, Len2, {_, _, Tree21, Tree22, Tree23, Cache2}}) when H2 == H1 + 1 ->
+merge(Tree1 = {H1, Len1, _}, {H2, Len2, {_, _, Tree21, Tree22, Tree23, Cache2}}, Params) when H2 == H1 + 1 ->
 	Cache1 = extract_tree_cache(Tree1),
 	Cache21 = extract_tree_cache(Tree21),
 	Cache22 = extract_tree_cache(Tree22),
 	Cache23 = extract_tree_cache(Tree23),
+	{Module, _} = Params,
 	{_, Len21, _} = Tree21,
 	{_, Len22, _} = Tree22,
 	{_, Len23, _} = Tree23,
 	{H2 + 1, Len1 + Len2, % tree
 		{Len1 + Len21, % 2-node
-			{H2, Len1 + Len21, {Len1, Tree1, Tree21, merge_caches(Cache1, Cache21)}}, % tree with 2-node
-			{H2, Len22 + Len23, {Len22, Tree22, Tree23, merge_caches(Cache22, Cache23)}},
-			merge_caches(Cache1, Cache2)}}; % tree with 2-node
+			{H2, Len1 + Len21, {Len1, Tree1, Tree21, Module:merge_caches(Cache1, Cache21)}}, % tree with 2-node
+			{H2, Len22 + Len23, {Len22, Tree22, Tree23, Module:merge_caches(Cache22, Cache23)}},
+			Module:merge_caches(Cache1, Cache2)}}; % tree with 2-node
 
 % diff > 1
 % 2-node + small
-merge({H1, _, {_, Tree11, Tree12, _}}, {H2, Len2, Node2}) when H1 > H2 + 1 ->
+merge({H1, _, {_, Tree11, Tree12, _}}, {H2, Len2, Node2}, Params) when H1 > H2 + 1 ->
 	merge(
 		Tree11,
-		merge(Tree12, {H2, Len2, Node2}));
+		merge(Tree12, {H2, Len2, Node2}, Params), 
+		Params);
 % small + 2-node
-merge({H1, Len1, Node1}, {H2, _, {_, Tree21, Tree22, _}}) when H2 > H1 + 1 ->
+merge({H1, Len1, Node1}, {H2, _, {_, Tree21, Tree22, _}}, Params) when H2 > H1 + 1 ->
 	merge(
-		merge({H1, Len1, Node1}, Tree21),
-		Tree22);
+		merge({H1, Len1, Node1}, Tree21, Params),
+		Tree22,
+		Params);
 % 3-node + small
-merge({H1, _, {_, _, Tree11, Tree12, Tree13, _}}, Tree2 = {H2, _, _}) when H1 > H2 + 1 ->
+merge({H1, _, {_, _, Tree11, Tree12, Tree13, _}}, Tree2 = {H2, _, _}, Params) when H1 > H2 + 1 ->
 	merge(
-		merge(Tree11, Tree12),
-		merge(Tree13, Tree2));
+		merge(Tree11, Tree12, Params),
+		merge(Tree13, Tree2, Params),
+		Params);
 % small + 3-node
-merge(Tree1 = {H1, _, _}, {H2, _, {_, _, Tree21, Tree22, Tree23, _}}) when H2 > H1 + 1 ->
+merge(Tree1 = {H1, _, _}, {H2, _, {_, _, Tree21, Tree22, Tree23, _}}, Params) when H2 > H1 + 1 ->
 	merge(
-		merge(Tree1, Tree21),
-		merge(Tree22, Tree23)).
+		merge(Tree1, Tree21, Params),
+		merge(Tree22, Tree23, Params),
+		Params).
 
 
+% % public
+-spec flatten(rope(), params()) -> value() | undefined.
 
+flatten(empty_rope, _) ->
+	undefined;
+flatten(Tree, Params) ->
+	flatten(Tree, undefined, Params).
 
-%% to start with
--spec empty_rope() -> rope().
-
-empty_rope() -> empty_rope.
-
-
-
-
--spec binary2rope(binary()) -> rope().
-
-binary2rope(Binary) ->
-	binary2rope(Binary, empty_rope).
-
-binary2rope(<<>>, AccRope) ->
-	AccRope;
-binary2rope(Binary, AccRope) when byte_size(Binary) =< ?Leaf_size ->
-	Leaf = {Binary, compute_cache(Binary)},
-	merge(AccRope, {0, byte_size(Binary), Leaf});
-binary2rope(Binary, AccRope) ->
-	<<LeftChunk:?Leaf_size/binary, RestBinary/binary>> = Binary,
-	Leaf = {LeftChunk, compute_cache(LeftChunk)},
-	binary2rope(RestBinary, merge(AccRope, {0, ?Leaf_size, Leaf})).
-
-
-
-
--spec rope2binary(rope()) -> binary().
-
-rope2binary(empty_rope) ->
-	<<>>;
-rope2binary(Tree) ->
-	rope2binary(Tree, <<>>).
-
-rope2binary(empty_rope, AccBinary) ->
+flatten(empty_rope, AccBinary, _) ->
 	AccBinary;
-rope2binary({0, _, {Val, _}}, AccBinary) ->
-	<<AccBinary/binary, Val/binary>>;
-rope2binary({_, _, {_, Tree1, Tree2, _}}, AccBinary) ->
-	LeftBinary = rope2binary(Tree1, AccBinary),
-	rope2binary(Tree2, LeftBinary);
-rope2binary({_, _, {_, _, Tree1, Tree2, Tree3, _}}, AccBinary) ->
-	LeftBinary = rope2binary(Tree1, AccBinary),
-	MiddleBinary = rope2binary(Tree2, LeftBinary),
-	rope2binary(Tree3, MiddleBinary).
+flatten({0, _, {Val, _}}, undefined, _) ->
+	Val;
+flatten({0, _, {Val, _}}, AccBinary, {Module, _}) ->
+	Module:merge_values(AccBinary, Val);
+flatten({_, _, {_, Tree1, Tree2, _}}, AccBinary, Params) ->
+	LeftBinary = flatten(Tree1, AccBinary, Params),
+	flatten(Tree2, LeftBinary, Params);
+flatten({_, _, {_, _, Tree1, Tree2, Tree3, _}}, AccBinary, Params) ->
+	LeftBinary = flatten(Tree1, AccBinary, Params),
+	MiddleBinary = flatten(Tree2, LeftBinary, Params),
+	flatten(Tree3, MiddleBinary, Params).
 
 
-
-
+% public
 -spec length(rope()) -> non_neg_integer().
 
 length(empty_rope) -> 0;
 length({_, Len, _}) -> Len.	
 
-
-
-
-
-compute_cache(Binary) -> dummy_cache.
-
-merge_caches(Cache1, Cache2) -> dummy_cache.
 
 extract_cache({_, _, _, _, _, Cache}) -> Cache;
 extract_cache({_, _, _, Cache}) -> Cache;
--- rope/rope_test.erl
+++ rope/rope_test.erl
@@ -1,12 +1,76 @@
 -module(rope_test).
 -export([test/0]).
 
+-behaviour(rope).
+-export([length/1, cache/1, merge_values/2, split_value/2, merge_caches/2]).
+
+-type cache() :: cache.
+-type value() :: binary().
+
+
+%%% rope callbacks %%%
+
+-spec length(value()) -> number().
+
+length(BinString) -> byte_size(BinString).
+
+
+-spec cache(value()) -> cache().
+
+cache(_) -> cache.
+
+
+% length(Value1) + length(Value2) = length(merge_values(Value1, Value2))
+-spec merge_values(value(), value()) -> value().
+
+merge_values(BinString1, BinString2) ->
+	<<BinString1/binary, BinString2/binary>>.
+
+
+% if {Value1, Value2} = split_value(Value) 
+% then length(Value1) + length(Value2) = length(Value)
+-spec split_value(value(), number()) -> {value(), value()}.
+
+split_value(BinString, Len) ->
+	<<BinString1:Len/binary, BinString2/binary>> = BinString,
+	{BinString1, BinString2}.
+
+
+-spec merge_caches(cache(), cache()) -> cache().
+% dummy cache
+merge_caches(cache, cache) -> cache.
+
+
+%%% Test %%%%
+
+params() -> {?MODULE, 5}.
+
+
+-spec binary2rope(binary()) -> rope:rope().
+
+binary2rope(Binary) ->
+	binary2rope(Binary, rope:leaf_rope(<<"">>, params())).
+
+binary2rope(<<>>, AccRope) ->
+	AccRope;
+binary2rope(Binary, AccRope) ->
+	{_, LeafLength} = params(),
+	case byte_size(Binary) =< LeafLength of 
+		true ->
+			NewRope = rope:leaf_rope(Binary, params()),
+			rope:merge(AccRope, NewRope, params());
+		false ->
+			<<LeftChunk:LeafLength/binary, RestBinary/binary>> = Binary,
+			NewRope = rope:leaf_rope(LeftChunk, params()),
+			binary2rope(RestBinary, rope:merge(AccRope, NewRope, params()))
+	end.
+
+
 test() ->
 	io:format("Rope test begins~n"),
 	test_conversion_equality(),
 	test_merge_split_equality(),
 	io:format("Rope test ends~n").
-
 
 
 test_merge_split_equality() ->
@@ -20,17 +84,30 @@ test_merge_split_equality() ->
 		<<>>,
 		lists:flatten(lists:duplicate(17, "1234567"))).
 
+
 test_single_right_to_left(Binary) ->
 	lists:foreach(
 		fun(Pos) ->
-			Rope = rope:binary2rope(Binary),
-			{Rope1, Rope2} = rope:split(Rope, Pos),
-			Binary1 = rope:rope2binary(Rope1),
-			Binary2 = rope:rope2binary(Rope2),
-			Rope12 = rope:merge(Rope1, Rope2),
-			Rope21 = rope:merge(Rope2, Rope1),
-			Binary34 = rope:rope2binary(Rope12),
-			Binary43 = rope:rope2binary(Rope21),
+			Rope = binary2rope(Binary),
+			{Rope1, Rope2} = rope:split(Rope, Pos, params()),
+			Binary1 = case rope:flatten(Rope1, params()) of 
+				undefined -> <<>>;
+				Val1 -> Val1
+			end,
+			Binary2 = case rope:flatten(Rope2, params()) of 
+				undefined -> <<>>;
+				Val2 -> Val2
+			end,
+			Rope12 = rope:merge(Rope1, Rope2, params()),
+			Rope21 = rope:merge(Rope2, Rope1, params()),
+			Binary34 = case rope:flatten(Rope12, params()) of 
+				undefined -> <<>>;
+				Val34 -> Val34
+			end,
+			Binary43 = case rope:flatten(Rope21, params()) of 
+				undefined -> <<>>;
+				Val43 -> Val43
+			end,
 
 			Binary12 = <<Binary1/binary, Binary2/binary>>,
 			Binary21 = <<Binary2/binary, Binary1/binary>>,
@@ -47,10 +124,10 @@ test_single_right_to_left(Binary) ->
 test_single_merge_split(Binary) ->
 	lists:foreach(
 		fun(Pos) ->
-			Rope = rope:binary2rope(Binary),
-			{Rope1, Rope2} = rope:split(Rope, Pos),
-			Rope3 = rope:merge(Rope1, Rope2),
-			Binary2 = rope:rope2binary(Rope3),
+			Rope = binary2rope(Binary),
+			{Rope1, Rope2} = rope:split(Rope, Pos, params()),
+			Rope3 = rope:merge(Rope1, Rope2, params()),
+			Binary2 = rope:flatten(Rope3, params()),
 			Result = Binary == Binary2,
 			maybe_print(not Result, "fail with:~n" ++ to_list(Binary) ++ "~n on pos: " ++ to_list(Pos)),
 			Result2 = rope:length(Rope) == (rope:length(Rope1) + rope:length(Rope2)),
@@ -72,14 +149,11 @@ test_conversion_equality() ->
 		<<>>,
 		lists:flatten(lists:duplicate(23, "1234567"))).
 
+
 test_single_conversion(Binary) ->
-	Rope = rope:binary2rope(Binary),
-	Binary2 = rope:rope2binary(Rope),
+	Rope = binary2rope(Binary),
+	Binary2 = rope:flatten(Rope, params()),
 	Binary2 == Binary.
-
-
-
-
 
 
 to_list(X) ->
