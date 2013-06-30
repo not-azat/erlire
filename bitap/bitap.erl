76ab29c702f244352c08c216972a5043daa60f43
Azat Abdulvaliev <atik1990@ya.ru>
2013-06-30 22:43:07 +0400

    bitap: matching with errors, but positioning not clear yet

--- bitap/bitap.erl
+++ bitap/bitap.erl
@@ -1,54 +1,97 @@
 -module(bitap).
 -compile(export_all).
--export([find/2]).
+-export([find/2, find/3]).
 
--spec find(Pattern::binary(), Bitstring::binary()) -> [number()].
+-define(sl1(X), ((X bsl 1) bor 1)).
 
-find(Pattern, Bitstring) ->
+-spec find(Pattern::binary(), Str::binary()) -> [number()].
+
+find(Pattern, Str) ->
+	find(Pattern, Str, 0).
+
+
+-spec find(Pattern::binary(), Str::binary(), K::number()) -> [number()].
+
+find(Pattern, Str, K) ->
 	Masks = compile_masks(Pattern),
 	lists:reverse(
-		match(Bitstring, Masks, size(Pattern) - 1)).
+		match(Str, Masks, size(Pattern) - 1, K)).
 
 
--spec compile_masks(Pattern::binary()) -> dict().
-%% dict(Letter => Mask::binary()).
+-spec compile_masks(Pattern::binary()) -> [{Letter::char(), Mask::binary()}].
+
 compile_masks(<<>>) ->
 	throw('bad argument');
 compile_masks(Pattern) ->
-	compile_masks(Pattern, 0, dict:new()).
+	compile_masks(Pattern, 0, []).
 
 compile_masks(<<>>, _, AccDict) ->
 	AccDict;
-compile_masks(<<Letter,Rest/binary>>, I, AccDict) ->
-	M1 = case (dict:find(Letter, AccDict)) of 
-		{ok, M} -> M;
-		error -> 0
+compile_masks(<<Letter,Rest/binary>>, J, AccDict) ->
+	M1 = case (lists:keyfind(Letter, 1, AccDict)) of 
+		{Letter, M} -> M;
+		false -> 0
 	end,
-	AccDict1 = dict:store(Letter, M1 bor (1 bsl I), AccDict),
-	compile_masks(Rest, I + 1, AccDict1).
+	AccDict1 = lists:keystore(Letter, 1, AccDict, {Letter, M1 bor (1 bsl J)}),
+	compile_masks(Rest, J + 1, AccDict1).
 
 
--spec match(Bitstring::binary(), Masks::dict(), N::number()) -> [{Pos::number(), Len::number()}].
+-spec match(Str::binary(), Masks::dict(), N::number(), K::number()) 
+		-> [{Pos::number(), Len::number(), EditDistance::number()}].
 
-match(Bitstring, Masks, N) ->
-	match(Bitstring, Masks, 0, 0, N, []).
+match(<<>>, _, _, _) ->
+	[];
+match(Str, Masks, N, K) ->
+	match0(Str, Masks, 0, [ 0 || _ <- lists:seq(0, K) ], N, K, []).
 
-match(<<>>, _Masks, _I, _R, _N, ResultsAcc) ->
+
+% K = 0 (zero distance between substr and pattern)
+match0(<<>>, _Masks, _J, _Rls, _N, _KMax, ResultsAcc) ->
+	io:format("match0:~n\tStr: ~p~n\tMasks: ~p~n\tJ: ~p~n\tRls: ~p~n\tN: ~p~n\tKMax: ~p~n\tResultsAcc: ~p~n",
+		[<<>>, _Masks, _J, _Rls, _N, _KMax, ResultsAcc]),
 	ResultsAcc;
-match(<<Letter, Rest/binary>>, Masks, I, R, N, ResultsAcc) ->
-	M1 = case (dict:find(Letter, Masks)) of 
-		{ok, M} -> M;
-		error -> 0
+match0(<<Letter, Rest/binary>>, Masks, J, [Rl|Rls], N, KMax, ResultsAcc) ->
+	io:format("match0:~n\tStr: ~p~n\tMasks: ~p~n\tJ: ~p~n\tRls: ~p~n\tN: ~p~n\tKMax: ~p~n\tResultsAcc: ~p~n",
+		[<<Letter, Rest/binary>>, Masks, J, [Rl|Rls], N, KMax, ResultsAcc]),
+	M = get_mask(Letter, Masks),
+	io:format("\tnew M: ~p~n", [M]),
+	Rc = ?sl1(Rl) band M,
+	io:format("\tnew Rc: ~p~n", [Rc]),
+	NMask = 1 bsl N, % TODO move to params
+	io:format("\tnew Mask: ~p~n", [NMask]),
+	ResultsAcc1 = case (Rc band NMask) of 
+		NMask -> [{J-N, N+1, 0} | ResultsAcc];
+		_ -> ResultsAcc
 	end,
-	R1 = ((R bsl 1) bor 1) band M1,
-	NMask = 1 bsl N,
-	case (R1 band NMask) of 
-		NMask	-> match(Rest, Masks, I + 1, R1, N, [{I - N, N + 1} | ResultsAcc]);
-		_ 		-> match(Rest, Masks, I + 1, R1, N, ResultsAcc)
+	matchK(<<Letter, Rest/binary>>, Masks, J, 1, [Rl|Rls], [Rc], N, KMax, ResultsAcc1).
+
+
+matchK(<<>>, _Masks, _J, _K, _Rls, _Rcs, _N, _KMax, ResultsAcc) ->
+	io:format("matchK:~n\tStr: ~p~n\tMasks: ~p~n\tJ: ~p~n\tK: ~p~n\tRls: ~p~n\tRcs: ~p~n\tN: ~p~n\tKMax: ~p~n\tResultsAcc: ~p~n",
+		[<<>>, _Masks, _J, _K, _Rls, _Rcs, _N, _KMax, ResultsAcc]),
+	ResultsAcc;
+matchK(<<Letter, Rest/binary>>, Masks, J, K, _Rls, Rcs, N, KMax, ResultsAcc) when K > KMax ->
+	io:format("matchK:~n\tStr: ~p~n\tMasks: ~p~n\tJ: ~p~n\tK: ~p~n\tRls: ~p~n\tRcs: ~p~n\tN: ~p~n\tKMax: ~p~n\tResultsAcc: ~p~n",
+		[<<Letter, Rest/binary>>, Masks, J, K, _Rls, Rcs, N, KMax, ResultsAcc]),
+	match0(Rest, Masks, J + 1, lists:reverse(Rcs), N, KMax, ResultsAcc);
+matchK(<<Letter, Rest/binary>>, Masks, J, K, [Rl0, Rl1 | Rls], [Rc1 | Rcs], N, KMax, ResultsAcc) ->	
+	io:format("matchK:~n\tStr: ~p~n\tMasks: ~p~n\tJ: ~p~n\tK: ~p~n\tRls: ~p~n\tRcs: ~p~n\tN: ~p~n\tKMax: ~p~n\tResultsAcc: ~p~n",
+		[<<Letter, Rest/binary>>, Masks, J, K, [Rl0, Rl1 | Rls], [Rc1 | Rcs], N, KMax, ResultsAcc]),
+	M = get_mask(Letter, Masks),
+	Rc = ?sl1(Rl1) band M bor ?sl1(Rl0 bor Rc1) bor Rl0,
+	NMask = 1 bsl N, % TODO move to params
+	ResultsAcc1 = case (Rc band NMask) of 
+		NMask -> [{J-N, N+1, K} | ResultsAcc];
+		_ -> ResultsAcc
+	end,
+	matchK(<<Letter, Rest/binary>>, Masks, J, K+1, [Rl1 | Rls], [Rc, Rc1 | Rcs], N, KMax, ResultsAcc1).
+
+
+get_mask(Letter, Masks) ->
+	case (lists:keyfind(Letter, 1, Masks)) of 
+		{Letter, Mask} -> Mask;
+		false -> 0
 	end.
-
-
-
 
 
 
--- bitap/bitap_tests.erl
+++ bitap/bitap_tests.erl
@@ -1,24 +1,39 @@
 -module(bitap_tests).
--import(bitap, [find/2]).
+-import(bitap, [find/2, find/3]).
 -include_lib("eunit/include/eunit.hrl").
 
 find_test_() ->
 	[
-		f(<<"abcde">>, <<"abcd">>, []),
-		f(<<"abcde">>, <<"abcde">>, [{0,5}]),
-		f(<<"abcde">>, <<"abcdeabcde">>, [{0,5}, {5,5}]),
-		f(<<"abcde">>, <<"abcabcde">>, [{3,5}]),
-		f(<<"aaa">>, <<"aaaa">>, [{0,3}, {1,3}]),
-		f(<<"aaa">>, <<"nnaabaabaaaaa">>, [{8,3}, {9,3}, {10,3}])
+		f0(<<"abcde">>, <<"abcd">>, []),
+		f0(<<"abcde">>, <<"abcde">>, [{0,5,0}]),
+		f0(<<"abcde">>, <<"abcdeabcde">>, [{0,5,0}, {5,5,0}]),
+		f0(<<"abcde">>, <<"abcabcde">>, [{3,5,0}]),
+		f0(<<"aaa">>, <<"aaaa">>, [{0,3,0}, {1,3,0}]),
+		f0(<<"aaa">>, <<"nnaabaabaaaaa">>, [{8,3,0}, {9,3,0}, {10,3,0}])
+	].
+
+find_k_test_() ->
+	[
+		fk(<<"abcde">>, <<"abcd">>, 1, have),
+		fk(<<"abcde">>, <<"ab">>, 1, not_have),
+		fk(<<"abcd">>, <<"asbfced">>, 1, not_have),
+		fk(<<"abcd">>, <<"asbfced">>, 2, not_have),
+		fk(<<"abcd">>, <<"asbfced">>, 3, have),
+		fk(<<"abc">>, <<"aff">>, 1, not_have)
 	].
 
 
 empty_str_test_() ->
-	f(<<"any">>, <<>>, []).
+	f0(<<"any">>, <<>>, []).
 
 
-f(Pattern, Str, Positions) ->
+f0(Pattern, Str, Positions) ->
 	?_assertEqual(Positions, find(Pattern, Str)).
+
+fk(Pattern, Str, K, have) ->
+	?_assertNotEqual([], find(Pattern, Str, K));
+fk(Pattern, Str, K, not_have) ->
+	?_assertEqual([], find(Pattern, Str, K)).
 
 
 empty_pattern_test_() ->
