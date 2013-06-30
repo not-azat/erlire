f090c14b71a6f23263fcb7cfb801f68ab7537861
Azat Abdulvaliev <atik1990@ya.ru>
2013-07-01 00:50:53 +0400

    allow binary traversing optimizations (10x faster), removed io:format's

--- bitap/bitap.erl
+++ bitap/bitap.erl
@@ -39,59 +39,48 @@ compile_masks(<<Letter,Rest/binary>>, J, AccDict) ->
 -spec match(Str::binary(), Masks::dict(), N::number(), K::number()) 
 		-> [{Pos::number(), Len::number(), EditDistance::number()}].
 
-match(<<>>, _, _, _) ->
-	[];
 match(Str, Masks, N, K) ->
-	match0(Str, Masks, 0, [ 0 || _ <- lists:seq(0, K) ], N, K, []).
+	match0(Str, Masks, 0, 0, [ 0 || _ <- lists:seq(0, K) ], [], N, K, []).
 
 
-% K = 0 (zero distance between substr and pattern)
-match0(<<>>, _Masks, _J, _Rls, _N, _KMax, ResultsAcc) ->
-	io:format("match0:~n\tStr: ~p~n\tMasks: ~p~n\tJ: ~p~n\tRls: ~p~n\tN: ~p~n\tKMax: ~p~n\tResultsAcc: ~p~n",
-		[<<>>, _Masks, _J, _Rls, _N, _KMax, ResultsAcc]),
+%% K = 0 (zero distance between substr and pattern)
+match0(<<>>, _Masks, _Mask, _J, _Rls, _Rcs, _N, _KMax, ResultsAcc) ->
+	% io:format("match0:~n\tStr: ~p~n\tMasks: ~p~n\tMask: ~p~n\tJ: ~p~n\tRls: ~p~n\tRcs: ~p~n\tN: ~p~n\tKMax: ~p~n\tResultsAcc: ~p~n",
+	% 	[<<>>, _Masks, _Mask, _J, _Rls, _Rcs, _N, _KMax, ResultsAcc]),
 	ResultsAcc;
-match0(<<Letter, Rest/binary>>, Masks, J, [Rl|Rls], N, KMax, ResultsAcc) ->
-	io:format("match0:~n\tStr: ~p~n\tMasks: ~p~n\tJ: ~p~n\tRls: ~p~n\tN: ~p~n\tKMax: ~p~n\tResultsAcc: ~p~n",
-		[<<Letter, Rest/binary>>, Masks, J, [Rl|Rls], N, KMax, ResultsAcc]),
-	M = get_mask(Letter, Masks),
-	io:format("\tnew M: ~p~n", [M]),
+%% K = 0 (zero distance between substr and pattern)
+match0(<<Letter, Rest/binary>>, Masks, _Mask, J, [Rl|Rls], _Rcs, N, KMax, ResultsAcc) ->
+	% io:format("match0:~n\tStr: ~p~n\tMasks: ~p~n\tMask: ~p~n\tJ: ~p~n\tRls: ~p~n\tRcs: ~p~n\tN: ~p~n\tKMax: ~p~n\tResultsAcc: ~p~n",
+	% 	[<<Letter, Rest/binary>>, Masks, _Mask, J, [Rl|Rls], _Rcs, N, KMax, ResultsAcc]),
+	M = case (lists:keyfind(Letter, 1, Masks)) of 
+		{Letter, Mask} -> Mask;
+		false -> 0
+	end,
+	% io:format("\tnew M: ~p~n", [M]),
 	Rc = ?sl1(Rl) band M,
-	io:format("\tnew Rc: ~p~n", [Rc]),
+	% io:format("\tnew Rc: ~p~n", [Rc]),
 	NMask = 1 bsl N, % TODO move to params
-	io:format("\tnew Mask: ~p~n", [NMask]),
+	% io:format("\tnew Mask: ~p~n", [NMask]),
 	ResultsAcc1 = case (Rc band NMask) of 
 		NMask -> [{J-N, N+1, 0} | ResultsAcc];
 		_ -> ResultsAcc
 	end,
-	matchK(<<Letter, Rest/binary>>, Masks, J, 1, [Rl|Rls], [Rc], N, KMax, ResultsAcc1).
+	{ResultsAcc2, Rcs2} = matchK(M, J, 1, [Rl|Rls], [Rc], N, KMax, ResultsAcc1),
+	match0(Rest, Masks, M, J+1, lists:reverse(Rcs2), [Rc], N, KMax, ResultsAcc2).
 
-
-matchK(<<>>, _Masks, _J, _K, _Rls, _Rcs, _N, _KMax, ResultsAcc) ->
-	io:format("matchK:~n\tStr: ~p~n\tMasks: ~p~n\tJ: ~p~n\tK: ~p~n\tRls: ~p~n\tRcs: ~p~n\tN: ~p~n\tKMax: ~p~n\tResultsAcc: ~p~n",
-		[<<>>, _Masks, _J, _K, _Rls, _Rcs, _N, _KMax, ResultsAcc]),
-	ResultsAcc;
-matchK(<<Letter, Rest/binary>>, Masks, J, K, _Rls, Rcs, N, KMax, ResultsAcc) when K > KMax ->
-	io:format("matchK:~n\tStr: ~p~n\tMasks: ~p~n\tJ: ~p~n\tK: ~p~n\tRls: ~p~n\tRcs: ~p~n\tN: ~p~n\tKMax: ~p~n\tResultsAcc: ~p~n",
-		[<<Letter, Rest/binary>>, Masks, J, K, _Rls, Rcs, N, KMax, ResultsAcc]),
-	match0(Rest, Masks, J + 1, lists:reverse(Rcs), N, KMax, ResultsAcc);
-matchK(<<Letter, Rest/binary>>, Masks, J, K, [Rl0, Rl1 | Rls], [Rc1 | Rcs], N, KMax, ResultsAcc) ->	
-	io:format("matchK:~n\tStr: ~p~n\tMasks: ~p~n\tJ: ~p~n\tK: ~p~n\tRls: ~p~n\tRcs: ~p~n\tN: ~p~n\tKMax: ~p~n\tResultsAcc: ~p~n",
-		[<<Letter, Rest/binary>>, Masks, J, K, [Rl0, Rl1 | Rls], [Rc1 | Rcs], N, KMax, ResultsAcc]),
-	M = get_mask(Letter, Masks),
-	Rc = ?sl1(Rl1) band M bor ?sl1(Rl0 bor Rc1) bor Rl0,
+%% K /= 0
+matchK(_Mask, _J, K, _Rls, Rcs, _N, KMax, ResultsAcc) when K > KMax ->
+	{ResultsAcc, Rcs};	
+matchK(Mask, J, K, [Rl0, Rl1 | Rls], [Rc1 | Rcs], N, KMax, ResultsAcc) ->	
+	% io:format("matchK:~n\tMask: ~p~n\tJ: ~p~n\tK: ~p~n\tRls: ~p~n\tRcs: ~p~n\tN: ~p~n\tKMax: ~p~n\tResultsAcc: ~p~n",
+	% 	[Mask, J, K, [Rl0, Rl1 | Rls], [Rc1 | Rcs], N, KMax, ResultsAcc]),
+	Rc = ?sl1(Rl1) band Mask bor ?sl1(Rl0 bor Rc1) bor Rl0,
 	NMask = 1 bsl N, % TODO move to params
 	ResultsAcc1 = case (Rc band NMask) of 
 		NMask -> [{J-N, N+1, K} | ResultsAcc];
 		_ -> ResultsAcc
 	end,
-	matchK(<<Letter, Rest/binary>>, Masks, J, K+1, [Rl1 | Rls], [Rc, Rc1 | Rcs], N, KMax, ResultsAcc1).
-
-
-get_mask(Letter, Masks) ->
-	case (lists:keyfind(Letter, 1, Masks)) of 
-		{Letter, Mask} -> Mask;
-		false -> 0
-	end.
+	matchK(Mask, J, K+1, [Rl1 | Rls], [Rc, Rc1 | Rcs], N, KMax, ResultsAcc1).
 
 
 
